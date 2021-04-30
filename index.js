316ab6c7fdf03813431b373fd0ae928f13f12860
Lucius Fowler <luciusfow@gmail.com>
2021-04-29 22:48:38 -0400

    commit tree 2e41a21536b0207ffb801a8f576752eac8861798

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin(array){
+    let found = array.find(value => value.result === "W")
+    if (found){
+        return found.year
+    } else{
+        return undefined
+    }
+}
+
